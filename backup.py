870bf16f0f342fa6395bff6b8f8d3bc2d34bf01a
ahir jere <ahirjere16@gmail.com>
2018-09-13 20:46:16 +0530

    made backup script

--- backup.py
+++ backup.py
@@ -2,5 +2,5 @@ import os
 
 
 os.system('git add --all')
-os.system('git commit -m "{}"'.format(input('Enter comment for git commit')))
+os.system('git commit -m "{}"'.format(input('Enter comment for git commit: ')))
 os.system('git push origin master')
